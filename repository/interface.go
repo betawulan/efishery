c177cb2e851e9b60d31fa779f99fbabc8bde6c32
t-beta.wulandari <t-beta.wulandari@virgoku.id>
2023-05-16 20:32:38 +0700

    feat: add interface auth repository

--- repository/interface.go
+++ repository/interface.go
@@ -10,3 +10,7 @@ type RegisterRepository interface {
 	Register(ctx context.Context, register model.Register) error
 	GetUser(ctx context.Context, filter model.RegisterFilter) (model.Register, error)
 }
+
+type AuthRepository interface {
+	Login(ctx context.Context, phone string, password string) (model.Register, error)
+}
