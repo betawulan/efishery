5ca33228e7d1f870d6bfe4947e65bc3e8443e9c6
t-beta.wulandari <t-beta.wulandari@virgoku.id>
2023-05-16 20:35:23 +0700

    feat: add interface auth service

--- service/interface.go
+++ service/interface.go
@@ -9,3 +9,7 @@ import (
 type RegisterService interface {
 	Register(ctx context.Context, register model.Register) (model.RegisterResponse, error)
 }
+
+type AuthService interface {
+	Login(ctx context.Context, phone string, password string) (string, error)
+}
