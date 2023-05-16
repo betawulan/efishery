35725b1b94c363627878aaa532fa230c1dcb48f3
t-beta.wulandari <t-beta.wulandari@virgoku.id>
2023-05-16 20:40:59 +0700

    feat: init new instance main

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/spf13/viper"
 
 	"github.com/betawulan/efishery/delivery"
+	"github.com/betawulan/efishery/packages/auth"
 	"github.com/betawulan/efishery/repository"
 	"github.com/betawulan/efishery/service"
 )
@@ -28,12 +29,19 @@ func main() {
 	if err != nil {
 		log.Fatal("can't connect database")
 	}
+	secretKey := viper.GetString("secret_key")
 
 	registerRepo := repository.NewRegisterRepository(db)
 	registerService := service.NewRegisterService(registerRepo)
 
+	jwt := auth.NewAuth([]byte(secretKey))
+
+	authRepo := repository.NewAuthRepository(db)
+	authService := service.NewAuthService(authRepo, jwt)
+
 	e := echo.New()
 	delivery.AddRegisterRoute(registerService, e)
+	delivery.AddAuthRoute(authService, e)
 
 	e.Logger.Fatal(e.Start(":5050"))
 }
