55114ae833938644efec1481b24b2bb7e45d248b
t-beta.wulandari <t-beta.wulandari@virgoku.id>
2023-05-16 14:50:54 +0700

    feat: define struct Register

---
+++ model/register.go
@@ -0,0 +1,12 @@
+package model
+
+import "time"
+
+type Register struct {
+	ID        int64     `json:"-"`
+	Phone     string    `json:"phone"`
+	Name      string    `json:"name"`
+	Role      string    `json:"role"`
+	Password  string    `json:",omitempty"`
+	CreatedAt time.Time `json:"-"`
+}
