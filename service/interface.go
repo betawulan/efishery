9f795ac0d117743db0f3c60aa99e8092a30e8a54
t-beta.wulandari <t-beta.wulandari@virgoku.id>
2023-05-17 01:33:02 +0700

    feat: add interface validate at service

--- service/interface.go
+++ service/interface.go
@@ -8,4 +8,5 @@ import (
 type AuthService interface {
 	Register(ctx context.Context, register model.User) (model.UserResponse, error)
 	Login(ctx context.Context, phone string, password string) (string, error)
+	Validate(token string) (model.User, error)
 }
