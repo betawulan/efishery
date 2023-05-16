238d48eda393ad8f3ef2670dcca30ea599df524b
t-beta.wulandari <t-beta.wulandari@virgoku.id>
2023-05-17 03:36:26 +0700

    refactor: rename naming variable

--- repository/interface.go
+++ repository/interface.go
@@ -7,7 +7,7 @@ import (
 )
 
 type AuthRepository interface {
-	Register(ctx context.Context, register model.User) error
+	Register(ctx context.Context, user model.User) error
 	GetUser(ctx context.Context, filter model.UserFilter) (model.User, error)
 	Login(ctx context.Context, phone string, password string) (model.User, error)
 }
