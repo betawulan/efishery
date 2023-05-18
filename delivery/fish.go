3ee9857d05f25385a4b134a9fbb8039a0f719102
t-beta.wulandari <t-beta.wulandari@virgoku.id>
2023-05-18 19:50:13 +0700

    feat: add endpoint fetch at delivery

---
+++ delivery/fish.go
@@ -0,0 +1,45 @@
+package delivery
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/labstack/echo"
+
+	"github.com/betawulan/efishery/error_message"
+	"github.com/betawulan/efishery/service"
+)
+
+type fishDelivery struct {
+	fishService service.FishService
+	authService service.AuthService
+}
+
+func (f fishDelivery) fetch(c echo.Context) error {
+	token := c.Request().Header.Get("Authorization")
+	if token == "" {
+		return c.JSON(http.StatusUnauthorized, error_message.Unauthorized{Message: "please provide token"})
+	}
+
+	tokens := strings.Split(token, " ")
+	if len(tokens) < 2 {
+		return c.JSON(http.StatusUnauthorized, error_message.Unauthorized{Message: "format token invalid"})
+	}
+
+	if tokens[0] != "Bearer" {
+		return c.JSON(http.StatusUnauthorized, error_message.Unauthorized{Message: "no Bearer"})
+	}
+
+	fishes, err := f.fishService.GetDataStorages(tokens[1])
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+
+	return c.JSON(http.StatusOK, fishes)
+}
+
+func AddFishRoute(fish service.FishService, e *echo.Echo) {
+	handler := fishDelivery{fishService: fish}
+
+	e.GET("/app", handler.fetch)
+}
